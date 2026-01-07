5a6fe8d9f9fd022b0162bed58fedaab8140537cd
Sahar <sahar.mostafavi.1130@gmail.com>
2026-01-07 20:37:08 +0330

    Add ProjectController with index, show, and store methods for authenticated users

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,92 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-// class ProjectController extends Controller
-// {
-//     //
-// }
-
-
-
-
-
-
-
-//use App\Models\Project;
-
-// class ProjectController extends Controller
-// {
-//     public function index(Request $request)
-//     {
-//         // فقط پروژه‌های کاربر لاگین‌شده
-//         return response()->json(
-//             $request->user()->projects()->latest()->get()
-//         );
-//     }
-
-//     public function store(Request $request)
-//     {
-//         $data = $request->validate([
-//             'title' => 'required|string',
-//             'description' => 'nullable|string',
-//         ]);
-
-//         $project = $request->user()->projects()->create($data);
-
-//         return response()->json($project, 201);
-//     }
-// }
-
-
-
-
-
-
 use App\Models\Project;
+
 
 class ProjectController extends Controller
 {
     public function index(Request $request)
     {
-        return response()->json(
-            $request->user()->projects()->with('tasks')->get()
-        );
+        $projects = Project::where('user_id', $request->user()->id)->get();
+
+        return response()->json($projects, 200);
     }
 
+    public function show(Project $project)
+    {
+        abort_if($project->user_id !== auth()->id(), 403);
+
+        return response()->json($project, 200);
+    }
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'title' => 'required|string',
+        $request->validate([
+            'title' => 'required|string|max:255',
             'description' => 'nullable|string',
         ]);
-
-        $project = $request->user()->projects()->create($data);
-
+        $project = Project::create([
+            'user_id' => $request->user()->id,
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
         return response()->json($project, 201);
     }
-
-    //public function show(Project $project)
-    public function project()
-{
-    return $this->belongsTo(Project::class);
-
-        $this->authorizeOwner($project);
-        return response()->json($project->load('tasks'));
-    }
-
-    public function destroy(Project $project)
-    {
-        $this->authorizeOwner($project);
-        $project->delete();
-        return response()->json(['message'=>'Deleted']);
-    }
-
-    private function authorizeOwner(Project $project)
-    {
-        if ($project->user_id !== auth()->id()) {
-            abort(403,'Access denied');
-        }
-    }
 }
+
+
+
+
