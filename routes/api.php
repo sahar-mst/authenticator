6750fa83bb4c32dbdb66c45d2fcaa07da749df27
Sahar <sahar.mostafavi.1130@gmail.com>
2026-01-07 20:08:45 +0330

    Add API routes related to authentication, projects, and tasks

--- routes/api.php
+++ routes/api.php
@@ -1,172 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-use Carbon\Carbon;
-
-
-
-
-
-
-
-
-//Route::get('/time', function () {
-    //return response()->json([
-        //'current_time' => Carbon::now('Asia/Tehran')->toDateTimeString()
-   // ]);
-//});
-
-
-
-
-//use App\Http\Controllers\TimeController;
-
-//Route::get('/time', [TimeController::class, 'now']);
-
-
-
-
-
+//use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Route;
-
-//Route::get('/name', function () {
-   // return 'sahar';
-//});
-
-
-
-
-
-
-//use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\Api\NameController;
-
-//Route::get('/name', [NameController::class, 'index']);
-
-
-
-
-
-//use App\Http\Controllers\Api\NameController;
-
-//Route::get('/name', [NameController::class, 'index']);
-
-
-
-
-
-//use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\UserController;
-
-//Route::get('/first', [UserController::class, 'index']);
-
-
-
-
-
-//Route::post('/register', [UserController::class, 'register']);
-//Route::get('/users', [UserController::class, 'index']);
-
-
-
-
-
-
-
-//use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\UserController;
-
-
-
-
-
-
-//use Illuminate\Support\Facades\Route;
-
-
-//Route::get('/name', function(){
-//return response()->json([
-  // 'name'=>'sahar',
-  // 'fristname'=>'mst'
-//]);
-//});
-
-// routes/api.php
-//use App\Http\Controllers\Api\TimeController;
-//use Illuminate\Support\Facades\Route;
-
-//Route::get('/time', [TimeController::class, 'now']);
-
-
-
-
-// class Controller extends BaseController
-// {
-//     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-// }
-
-
-// use App\Http\Controllers\ProgressController;
-
-// Route::get('/progress', [ProgressController::class, 'index']);
-
-
-
-
-//get → نوع درخواست HTTP (GET، POST، PUT، DELETE و …)
-
-//ProgressController → کنترلری که مسئول این مسیر است
-
-//index → متدی در کنترلر که پاسخ را برمی‌گرداند
-
-
-
-
-
-
-
-// Route::post('/data',function(){
-// return response()->json(["course_name"=>"laravel"]);
-// });
-
-
-
-// Route::post('/data',function(Request $request){
-//    return response()->json ([
-//       "name_user" =>$request-> name,
-//    "email_user" =>$request-> email,
-// ]);
-// });
-
-// Route ::post ('/register',[UserController ::class, 'store']);
-
-
-
-
-//use App\Http\Controllers\ProjectController;
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/projects', [ProjectController::class, 'index']);
-//     Route::post('/projects', [ProjectController::class, 'store']);
-// });
-
-
-
-
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\ProjectController;
-Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class,'login']);
+use App\Http\Controllers\TaskController;
+
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('projects', ProjectController::class);
-    Route::post('/projects/{project}/tasks', [TaskController::class,'store']);
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::apiResource('projects',ProjectController::class);
+
+    Route::post('projects/{project}/tasks',[TaskController::class,'store']);
+
+    Route::get('projects/{project}/tasks', [TaskController::class, 'index']);
+    Route::get('/projects/{project}/tasks',[TaskController::class, 'show']);
 });
+
+
+
 
 
 
