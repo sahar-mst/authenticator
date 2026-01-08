7a78c376340319b52a5e3a94950fef352f7116d6
Sahar <sahar.mostafavi.1130@gmail.com>
2026-01-08 10:15:38 +0330

    Add ProjectController with index, show, and store methods for authenticated users

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,21 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
 
 
 class ProjectController extends Controller
 {
     public function index(Request $request)
+
     {
-        $projects = Project::where('user_id', $request->user()->id)->get();
+        $userId =  Auth::id();
+        $projects = Project::with('tasks')->where('user_id', $userId)->get();
 
         return response()->json($projects, 200);
     }
 
-    public function show(Project $project)
+    public function show($id)
     {
-        abort_if($project->user_id !== auth()->id(), 403);
+        $userId =  Auth::id();
+        $project = Project::with('tasks')->where('user_id',$userId)->where('id', $id)->first();
 
+        if (!$project) {
+            return response()->json([
+                'error' => 'Project not found or you do not have access.'
+            ], 404);
+
+        }
         return response()->json($project, 200);
     }
     public function store(Request $request)
@@ -34,7 +44,54 @@ class ProjectController extends Controller
         ]);
         return response()->json($project, 201);
     }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+        $userId =  Auth::id();
+        $project = Project::where('user_id',$userId)->where('id', $id)->first();
+
+        if (!$project) {
+            return response()->json([
+                'error' => 'Project not found or you do not have access.'
+            ], 404);
+
+        }
+
+        $project->update([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'message' => 'Project updated successfully.'
+        ], 200);
+    }
+
+
+    public function destroy($id)
+    {
+        $userId =  Auth::id();
+        $project = Project::where('user_id',$userId)->where('id', $id)->first();
+
+        if (!$project) {
+            return response()->json([
+                'error' => 'Project not found or you do not have access.'
+            ], 404);
+
+        }
+        $project->delete();
+
+        return response()->json([
+            'message' => 'Project deleted successfully.'
+        ], 200);
+    }
 }
+
+
+
 
 
 
