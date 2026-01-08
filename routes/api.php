38b7dce3db88a2454779b6027b98c7ba00ba136d
Sahar <sahar.mostafavi.1130@gmail.com>
2026-01-08 10:14:53 +0330

    "Add API paths related to authentication, projects, and tasks."

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\TaskController;
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',[AuthController::class,'logout']);
     Route::apiResource('projects',ProjectController::class);
